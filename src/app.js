b20b8447d0253f137f577e7182e64f127e315b83
asalam3312 <141695644+asalam3312@users.noreply.github.com>
2023-10-16 18:10:48 +0000

    added arrays & loops

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const pronoun = ["the", "our", "their", "mine"];
+const adj = ["great", "big", "small", "bored"];
+const noun = ["jogger", "racoon", "kangooro", "turtle"];
+
+const domain = [".com", ".net", ".org"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let o = 0; o < adj.length; o++) {
+    for (let p = 0; p < noun.length; p++) {
+      for (let d = 0; d < domain.length; d++)
+        console.log(pronoun[i] + "" + adj[o] + "" + noun[p] + "" + domain[d]);
+    }
+  }
+}
